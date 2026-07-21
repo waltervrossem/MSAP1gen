4dc89ffae94e4ae3cd520b0fac6ad6109a53cecf
waltervrossem <walter_v_rossem@hotmail.com>
2026-07-21 17:54:40 +0200

    Use Lu+2024 for rotations

--- src/MSAP1gen/generate_settings.py
+++ src/MSAP1gen/generate_settings.py
@@ -9,6 +9,7 @@ from wsssss import load_data as ld
 from wsssss import functions as uf
 from platoconstants import cgs
 from platoconstants import cs
+from scipy import interpolate as ip
 
 import common
 
@@ -30,10 +31,73 @@ for h in hists:
 
 iters_general = itertools.product(profs, Gmags, rel_rotations, inclinations, num_spots, transits)
 iters_special_case = []
+def S_logP_l(logP, wP, logP_break):
+    return 1 / (1 + np.exp(-(logP_break - logP)/wP))
+
+
+def S_logP_h(logP, wP, logP_break):
+    return 1 / (1 + np.exp(-(-logP_break + logP) / wP))
+
+
+def f_period(logP, logP_break, wP, dP1, dP2):
+    P = 10**logP
+    Pbreak = 10**logP_break
+    return S_logP_h(logP, wP, logP_break) * P**dP1 + S_logP_l(logP, wP, logP_break) * P**dP2 * Pbreak ** (dP1 - dP2)
+
+
+def S_T_h(Tn, wT):
+    return 1 / (1 + np.exp(-(1 - Tn)/wT))
+
+
+def S_T_l(Tn, wT):
+    return 1 / (1 + np.exp(-(-1 + Tn)/wT))
+
+
+def g_temp(Tn, wT, c_T, dT1, dT2):
+    return S_T_h(Tn, wT) * (Tn - c_T)**dT1 + S_T_l(Tn, wT) * (Tn - c_T)**dT2 * (1 - c_T)**(dT1 - dT2)
+
+def gyro_mean_function(logP, Teff, fully_convective):
+    if fully_convective:
+        a = 0.774
+        dP1 = 0.376
+        dP2 = 1.811
+        c_T = -0.223
+        dT1 = -0.687
+        dT2 = dT1
+        Pbreak = 73.322
+        Tbreak = 0
+        wT = 1
+        wP = 0.068
+        Tn = (3500 - Teff) / 500
+    else:
+        a = 118.969
+        dP1 = -0.405
+        dP2 = 1.822
+        c_T = -0.399
+        dT1 = 1.646
+        dT2 = -17.779
+        Pbreak = 100.836
+        Tbreak = 3713.699
+        wT = 0.062
+        wP = 0.111
+        Tn = (7000 - Teff) / (7000 - Tbreak)
+    logP_break = np.log10(Pbreak)
+    return a * f_period(logP, logP_break, wP, dP1, dP2) * g_temp(Tn, wT, c_T, dT1, dT2)
 
 
 def calc_rotation(p, rel_rotation):
-    return rel_rotation * 30
+    mass = p.header['star_mass']
+    L = p.header['photosphere_L']
+    age = p.header['star_age']
+    Teff = p.header['Teff']
+    radius = np.sqrt(L / (Teff/5772)**4)
+    fully_convective = np.all(p.get('mixing_type')[p.get('radius') < radius] == c_mesa.convective_mixing)
+
+    # Lu+2024 https://iopscience.iop.org/article/10.3847/1538-3881/ad28b9/pdf
+    logP = np.linspace(0.5, 2.3, 101)
+    ages = gyro_mean_function(logP, Teff, fully_convective)
+    rot = rel_rotation * 10**ip.interp1d(ages, logP, bounds_error=False, fill_value=tuple(logP[[0, -1]]))(min(age/1e9, 14))
+    return rot
 
 
 def get_transit_config(p, transit_type):
