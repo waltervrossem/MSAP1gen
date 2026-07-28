8303058cf20ba7f147770634af8794b5aca05d7e
waltervrossem <walter_v_rossem@hotmail.com>
2026-07-28 14:59:28 +0200

    Add logic for special cases

--- src/MSAP1gen/make_all_config.py
+++ src/MSAP1gen/make_all_config.py
@@ -7,8 +7,8 @@ import multiprocessing as mp
 
 from run import *
 
-input_dir = '../../configs/general'
-output_dir = '../../input'
+base_input_dir = '../../configs/'
+base_output_dir = '../../input'
 
 def worker(config_name):
     if config_name.startswith('1'):
@@ -26,17 +26,21 @@ if __name__ == "__main__":
     os.environ['OMP_NUM_THREADS'] = '1'
 
     skip_existing = True
-    if not skip_existing:
-        shutil.rmtree(output_dir, ignore_errors=True)
+    for kind in ['general']:#, 'special']:
+        print(f'Making {kind} inputs.')
+        output_dir = os.path.join(base_output_dir, kind)
+        input_dir = os.path.join(base_input_dir, kind)
+        if not skip_existing:
+            shutil.rmtree(output_dir, ignore_errors=True)
 
-    os.makedirs(output_dir, exist_ok=True)
+        os.makedirs(output_dir, exist_ok=True)
 
-    config_files = sorted(os.listdir(input_dir))
-    existing = sorted(os.listdir(output_dir))
+        config_files = sorted(os.listdir(input_dir))
+        existing = sorted(os.listdir(output_dir))
 
-    if skip_existing:
-        config_files = [c for c in config_files if c[:8] not in existing]
+        if skip_existing:
+            config_files = [c for c in config_files if c[:8] not in existing]
 
-    with mp.Pool(nworker) as pool, tqdm.tqdm(total=len(config_files)) as pbar:
-        for res in pool.imap_unordered(worker, config_files):
-            pbar.update(1)
+        with mp.Pool(nworker) as pool, tqdm.tqdm(total=len(config_files)) as pbar:
+            for res in pool.imap_unordered(worker, config_files):
+                pbar.update(1)
