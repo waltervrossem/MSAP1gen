a9daefbc770d8caac0f1a8c8c79301a58398c6eb
waltervrossem <walter_v_rossem@hotmail.com>
2026-07-31 16:38:12 +0200

    Treat flares as spots which become brighter

--- src/psls/psls-1.9/flares.py
+++ src/psls/psls-1.9/flares.py
@@ -22,11 +22,30 @@ def flare(t0, amp0, dur0, up_down, t_LC_):
     return LCflare
 
 
-def add_flares(LC, t_LC, t_flares, amp_flares, duration_flares, up_down):
-    for t0, amp0, dur0 in zip(t_flares, amp_flares, duration_flares):
-        tmin = max([0, t0 - 3 * up_down * dur0])
-        tmax = min([max(t_LC), t0 + 5 * dur0])
-        t_LC_ = t_LC[(t_LC > tmin) & (t_LC < tmax)]
-        LC[(t_LC > tmin) & (t_LC < tmax)] = LC[(t_LC > tmin) & (t_LC < tmax)] + flare(t0, amp0, dur0, up_down, t_LC_)
+def add_flares(LC, t_LC, t_flares, amp_flares, duration_flares, up_down, prot=0, incl=None, lat=None, lon=None):
+    if prot == 0:
+        for t0, amp0, dur0 in zip(t_flares, amp_flares, duration_flares):
+            tmin = max([0, t0 - 3 * up_down * dur0])
+            tmax = min([max(t_LC), t0 + 5 * dur0])
+            t_LC_ = t_LC[(t_LC > tmin) & (t_LC < tmax)]
+            LC[(t_LC > tmin) & (t_LC < tmax)] = LC[(t_LC > tmin) & (t_LC < tmax)] + flare(t0, amp0, dur0, up_down, t_LC_)
+    else:
+        t_LC_days = t_LC / 86400
+        for t0, amp0, dur0, lat0, lon0 in zip(t_flares, amp_flares, duration_flares, lat, lon):
+            tmin = max([0, t0 - 3 * up_down * dur0])
+            tmax = min([max(t_LC), t0 + 5 * dur0])
+            mask = (t_LC > tmin) & (t_LC < tmax)
+            t_LC_ = t_LC[mask]
+            t_LC_days_ = t_LC_days[mask]
+            spot = spotintime.OneSpot(t_LC_days_, np.log(prot), 0, 2, lat0, lon0, None, None, 0)
+            spot_profile = spot.dimming(incl, modul=0)
+            if spot_profile.max() > 0:
+                spot_profile = spot_profile / spot_profile.max()
+            else:
+                continue
+
+            flare_LC0 = flare(t0, amp0, dur0, up_down, t_LC_)
+            flare_LC = spot_profile * flare_LC0
+            LC[(t_LC > tmin) & (t_LC < tmax)] = LC[(t_LC > tmin) & (t_LC < tmax)] + flare_LC
 
     return LC
