d9e559d28e10448a54cc32726f18182d62dfd296
waltervrossem <walter_v_rossem@hotmail.com>
2026-07-31 17:02:34 +0200

    Put flux in relative flux

--- src/MSAP1gen/format.py
+++ src/MSAP1gen/format.py
@@ -8,7 +8,28 @@ def format_MSAP1_in(input_file, output_file):
         with h5py.File(output_file, 'w') as o:
             for key, value in f.attrs.items():
                 o.attrs[key] = value
+            # O: oscillations (ppm)
+            # G: granulation (ppm)
+            # A: (stochastic) activity (ppm)
+            # RSE: residual systematic error (ppm)
+            # R: random error (ppm)
+            # S: spot (normalized flux)
+            # T: transit (normalized flux)
+            # s = 10^(-6)
+            # INDIVIDUAL_LC = ( ( 1 + ( O + G + A ) * s ) * ( 1 + (RSE + R)*s ) * S * T – 1)/s
+            O = f['OSCILLATIONS_LC'][()]
+            G = f['GRANULATION_LC'][()]
+            A = 0
+            RSE = f['SYSTEMATICS_LC'][()]
+            R = f['RANDOM_LC'][()]
+            S = f['SPOT_LC'][()] * f['FLARES_LC']
+            if 'TRANSIT_LC' in f.keys():
+                T = f['TRANSIT_LC']
+            else:
+                T = 1
+            s = 1e-6
+            CAMGROUP_LC = ((1 + (O + G + A) * s) * np.mean(1 + (RSE + R) * s, axis=2) * S * T - 1) / s  # Within 1e-10 of PSLS output
             o.create_dataset('TIME', data=f['TIME'])
-            o.create_dataset('FLUX', data=np.mean(f['INDIVIDUAL_LC'][:, :, :, 0], axis=2))
+            o.create_dataset('FLUX', data=1+CAMGROUP_LC*s)
             if 'TRANSIT_LC' in f.keys():
                 o.create_dataset('TRANSIT_REL', data=np.mean(f['TRANSIT_LC'], axis=1))
