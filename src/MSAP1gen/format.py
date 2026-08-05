e7096b0693e7cfe09221a79cbda1a0b461f293f8
waltervrossem <walter_v_rossem@hotmail.com>
2026-08-05 17:19:22 +0200

    Set default value is none in hdf5 output

--- src/MSAP1gen/format.py
+++ src/MSAP1gen/format.py
@@ -18,22 +18,43 @@ def format_MSAP1_in(input_file, output_file):
             # s = 10^(-6)
             # INDIVIDUAL_LC = ( ( 1 + ( O + G + A ) * s ) * ( 1 + (RSE + R)*s ) * S * T – 1)/s
             s = 1e-6
-            O = f['OSCILLATIONS_LC'][()]
-            G = f['GRANULATION_LC'][()]
+            if 'OSCILLATIONS_LC' in f.keys():
+                O = f['OSCILLATIONS_LC'][()]
+            else:
+                O = 0
+            if 'GRANULATION_LC' in f.keys():
+                G = f['GRANULATION_LC'][()]
+            else:
+                G = 0
             if 'ACTIVITY_LC' in f.keys():
                 A = f['ACTIVITY_LC'][()]
             else:
-                A = 1
-            E = 1 + f['EXTERNAL_LC'][()] * s
-            # OGA = f['STELLAR_LC']
-            RSE = f['SYSTEMATICS_LC'][()]
-            R = f['RANDOM_LC'][()]
-            S = f['SPOT_LC'][()] * f['FLARES_LC']
+                A = 0
+            if 'EXTERNAL_LC' in f.keys():
+                E = 1 + f['EXTERNAL_LC'][()] * s
+            else:
+                E = 1
+            if 'SYSTEMATICS_LC' in f.keys():
+                RSE = f['SYSTEMATICS_LC'][()]
+            else:
+                RSE= np.zeros((1,1,1))  # Can't be scalar 0 due to mean in CAMGROUP_LC calculation
+            if 'RANDOM_LC' in f.keys():
+                R = f['RANDOM_LC'][()]
+            else:
+                R = np.zeros((1,1,1))  # Can't be scalar 0 due to mean in CAMGROUP_LC calculation
+            if 'SPOT_LC' in f.keys():
+                S = f['SPOT_LC'][()]
+            else:
+                S = 1
+            if 'FLARES_LC' in f.keys():
+                F = f['FLARES_LC'][()]
+            else:
+                F = 1
             if 'TRANSIT_LC' in f.keys():
                 T = f['TRANSIT_LC']
             else:
                 T = 1
-            CAMGROUP_LC = ((1 + (O + G + A) * s) * np.mean(1 + (RSE + R) * s, axis=2) * S * T * E - 1) / s  # Within 1e-10 of PSLS output
+            CAMGROUP_LC = ((np.ones(f['TIME'].shape) + (O + G + A) * s) * np.mean(1 + (RSE + R) * s, axis=2) * S * F * T * E - 1)  # Within 1e-10 of PSLS output
             o.create_dataset('TIME', data=f['TIME'])
             o.create_dataset('FLUX', data=1+CAMGROUP_LC*s)
             if 'TRANSIT_LC' in f.keys():
