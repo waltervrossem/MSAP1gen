972de203012dee4580558e9e1f4dabf241875aa1
waltervrossem <walter_v_rossem@hotmail.com>
2026-08-07 15:38:56 +0200

    Check if MESA is initiated before checking version

--- src/MESA/run_mesa.sh
+++ src/MESA/run_mesa.sh
@@ -3,18 +3,23 @@ set -e
 
 echo "Running MESA"
 date "+DATE: %Y-%m-%d%nTIME: %H:%M:%S"
-# Run with MESA 24081
-MESA_version=$(cat "$MESA_DIR/data/version_number")
-if [ "$MESA_version" != "r24.08.1" ]; then
-  echo "Must run with MESA 24.08.1"
-  exit 1
-fi
 
 profiles=(grid/*/LOGS/profile*.data)
 
 if [[ ${#profiles[@]} -eq 99 ]]; then
   echo Found correct number of profiles.
 else
+  # Run with MESA 24081
+  if [[ -n "$MESA_DIR" ]]; then
+    MESA_version=$(cat "$MESA_DIR/data/version_number")
+    if [ "$MESA_version" != "r24.08.1" ]; then
+      echo "Must run with MESA 24.08.1"
+      exit 1
+    fi
+  else
+    echo "MESA_DIR not set, set to r24.08.1"
+    exit 1
+  fi
   python base_grid.py
   cd grid/0000
   ./mk &> /dev/null
